baf1e823f109ef6214a4b0d3ef50fbd370b1a2e4
Oleg Kozyr <olegvkozyr@gmail.com>
2018-04-22 13:46:48 +0300

    View multiple channels ad save to file

--- mult_chan.py
+++ mult_chan.py
@@ -39,7 +39,10 @@ class RunFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack(txn)
-
+        tk.Label(self, text='dt, c').pack(side=tk.LEFT)
+        self.dt = tk.Entry(self, width=5)
+        self.dt.pack(side=tk.LEFT)
+        self.dt.insert(0, '0.01')
         tk.Label(self, text='Вибірка, с').pack(side=tk.LEFT)
         self.sample = tk.Entry(self, width=5)
         self.sample.pack(side=tk.LEFT)
@@ -50,7 +53,7 @@ class RunFrame(tk.Frame):
         self.fps.insert(0, '500') 
         tk.Button(master=self, 
                   text='Старт', 
-                  command=master._start).pack(side=tk.LEFT) 
+                  command=master.start).pack(side=tk.LEFT) 
         tk.Button(master=self, 
                   text='Вихід', 
                   command=self._quit).pack(side=tk.RIGHT) 
@@ -64,6 +67,9 @@ class RunFrame(tk.Frame):
 
     def get_fps(self):
         return eval(self.fps.get())
+
+    def get_dt(self):
+        return eval(self.dt.get())
 
 class SaveOpenFrame(tk.Frame):
     def __init__(self, master):
@@ -80,32 +86,29 @@ class SaveOpenFrame(tk.Frame):
         self.file_path.pack(side=tk.LEFT)
         tk.Button(master=self, 
                   text='Створити', 
-                  command=self._save2file).pack(side=tk.LEFT)  
+                  command=self._file_save).pack(side=tk.LEFT)  
         tk.Button(master=self, 
                   text='Відкрити', 
-                  command=self._openfile).pack(side=tk.RIGHT)  
+                  command=self._file_open).pack(side=tk.RIGHT)  
 
-    def _save2file(self):
+    def _file_save(self):
         self.pathVar.set(self.filedialog.asksaveasfilename(initialdir = "", \
                                            title = "Створити файл", \
                                            filetypes = (("Txt files","*.txt"),("all files","*.*"))))
 
-    def _open_file(self):
+    def _file_open(self):
         path = self.filedialog.askopenfilename(initialdir = "", \
                                            title = "Відкрити файл", \
-                                           filetypes = (("Txt files","*.txt"),("all files","*.*"))))
-        with open(path, 'r') as _file:
-            _file.redline()
-            _file.redline()
-            for f in _file:
-                s = f.readline()
+                                           filetypes = (("Txt files","*.txt"),("all files","*.*")))
 
-        chanNames = ''
+        chanNames = []
+        dt = 0.0
         with open(path, 'r') as f:
             f.readline().split()
-            chanNames = f.readline().split()[1:]
-        array = np.genfromtxt(path, skip_header=2)
-        
+            dt = eval(f.readline()[3:])
+            chanNames = f.readline().split()
+        data = np.genfromtxt(path, skip_header=3)
+        self.master.update_canvas([np.arange(0, data.shape[0]*dt, dt), data[:, 0], data[:, 1], data[:, 2]], chanNames)
 
 class Application(tk.Frame):
     def __init__(self, master):
@@ -113,7 +116,9 @@ class Application(tk.Frame):
         self.pack(tby)
         self._timer = None
         self.sampleLen = 0
-        self.isStarted = False           
+        self.dt = 0
+        self.isStarted = False    
+        self.channels = ['ch0', 'ch1', 'ch2']
         self._create_widgets()
 
     def _create_widgets(self):
@@ -121,41 +126,48 @@ class Application(tk.Frame):
         self.canvasFrame.pack(tby)
         self.runFrame = RunFrame(self)
         self.runFrame.pack(txn)
-        self.saveFrame = SaveFrame(self)
+        self.saveFrame = SaveOpenFrame(self)
         self.saveFrame.pack(txn)
 
-    def genarator(self):
-        t = np.linspace(0, self.sampleLen, 1001)
+    def _genarator(self):
+        t = np.arange(0, self.sampleLen, self.dt)
         ch0 = np.sin(2*np.pi*t) + np.random.randn(t.shape[0])/10
         ch1 = np.cos(2*np.pi*t) + np.random.randn(t.shape[0])/10
         ch2 = np.sin(2*np.pi*t) * np.cos(2*np.pi*t) + \
-                                 np.random.randn(t.shape[0])/10 
-        return np.transpose(np.vstack((t, ch0, ch1, ch2)))
+                                  np.random.randn(t.shape[0])/10 
+        #return np.transpose(np.vstack((t, ch0, ch1, ch2)))
+        return [t, ch0, ch1, ch2]
 
-    def _update_canvas(self, data, chanNames):
+    def update_canvas(self, data, chanNames):
         '''
            Update figure plots
         '''
         self.canvasFrame._ax.clear()
-
         self.canvasFrame._ax.plot(data[0], data[1], label=chanNames[0])
-        self.canvasFrame._ax.plot(data[0], data[2], label=chanNames[1]) 
-        self.canvasFrame._ax.plot(data[0], data[3], label=chanNames[2])  
+        self.canvasFrame._ax.plot(data[0], data[2], label=chanNames[1])
+        self.canvasFrame._ax.plot(data[0], data[3], label=chanNames[2])
         self.canvasFrame._ax.legend(loc=1)
         self.canvasFrame._ax.grid()
         self.canvasFrame._ax.figure.canvas.draw()
 
-    def _update_canvas2(self):
+    def _save2file(self, data):
         '''
-           Update figure plots and save them to file
+           Save data to file
         '''
-        t, ch0, ch1, ch2 = self._update_canvas()
-
-        for x0, x1, x2, x3 in zip(t, ch0, ch1, ch2):
-            self.saveFrame.fileHandle.write('{:.4f} {:.4f} {:.4f} {:.4f}{}'.format(x0, x1, x2, x3, '\n'))
+        for x0, x1, x2 in zip(data[1], data[2], data[3]):
+            self.saveFrame.fileHandle.write('{:.4f} {:.4f} {:.4f}{}'.format(x0, x1, x2, '\n'))
             self.saveFrame.fileHandle.flush()
 
-    def _start(self):
+    def _plot(self):
+        data = self._genarator()
+        self.update_canvas(data, self.channels)
+
+    def _plot_save(self):
+        data = self._genarator()
+        self.update_canvas(data, self.channels)
+        self._save2file(data)
+
+    def start(self):
         if self.isStarted:
             self._timer.stop()
             if self.saveFrame.fileHandle:
@@ -164,19 +176,21 @@ class Application(tk.Frame):
             self.isStarted = False
         else:
             self.sampleLen = self.runFrame.get_sample_size() 
+            self.dt = self.runFrame.get_dt()
             if self.saveFrame.checkVar.get() and self.saveFrame.pathVar.get():
                 self.saveFrame.fileHandle = open(self.saveFrame.pathVar.get(), 'w')                               
                 self.saveFrame.fileHandle.write('{}{}'.format(\
                     strftime("%a, %d %b %Y %H:%M:%S +0300", localtime()), '\n'))
-                self.saveFrame.fileHandle.write('{} {} {} {}{}'.format('t', 'ch0', 'ch1', 'ch2', '\n'))
+                self.saveFrame.fileHandle.write('dt={}{}'.format(self.dt, '\n'))
+                self.saveFrame.fileHandle.write('{} {} {}{}'.format('ch0', 'ch1', 'ch2', '\n'))
                 self.saveFrame.fileHandle.flush()                
                 self._timer = self.canvasFrame._canvas.new_timer(
                                   self.runFrame.get_fps(), \
-                                  [(self._update_canvas2, (), {})])
+                                  [(self._plot_save, (), {})])
             else:
                 self._timer = self.canvasFrame._canvas.new_timer(
                                   self.runFrame.get_fps(), \
-                                  [(self._update_canvas, (), {})])
+                                  [(self._plot, (), {})])
             self._timer.start()
             self.isStarted = True
 
